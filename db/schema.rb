3383799a315f7fa44eb8434cf7442b24c2635d44
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2020-11-08 15:22:34 +0900

    フォームヘルパーへ修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_112329) do
+ActiveRecord::Schema.define(version: 2020_11_08_054829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 2020_10_28_112329) do
     t.string "email"
     t.string "image_url"
     t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "regions", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
