a22514c4bb98bc887e795706b388756cd7c31278
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2020-11-09 22:19:03 +0900

    deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'experiences#index'
-
-  get '/users', to: 'users#index'
-  get '/users/new', to: 'users#new'
-  post '/users', to: 'users#create'
-  get '/users/:id/edit', to: 'users#edit'
-  patch '/users/:id' , to: 'users#update'
-  delete '/users/:id', to: 'users#destroy'
 
   get '/hosts', to: 'hosts#index'
   get '/hosts/new', to: 'hosts#new'
