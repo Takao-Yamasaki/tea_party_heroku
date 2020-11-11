bdd3f24b25644f925a1e2d6986a7fbad3b0a3b8b
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2020-11-11 22:19:07 +0900

    resourcesへ変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'experiences#index'
-
-  get '/hosts', to: 'hosts#index'
-  get '/hosts/new', to: 'hosts#new'
-  post '/hosts', to: 'hosts#create'
-  get '/hosts/:id/edit', to: 'hosts#edit'
-  patch '/hosts/:id', to: 'hosts#update'
-  delete '/hosts/:id', to: 'hosts#destroy'
-
-  get '/experiences', to: 'experiences#index'
-  get '/experiences/new', to: 'experiences#new'
-  post '/experiences', to: 'experiences#create'
-  get '/experiences/:id/edit', to: 'experiences#edit'
-  patch '/experiences/:id', to: 'experiences#update'
-  delete '/experiences/:id', to: 'experiences#destroy'
+  devise_for :users
+  resources :experiences
+  resources :host
 end
