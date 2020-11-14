4f52cef68096537cefa22af4bd46a607ccff731b
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2020-11-14 19:44:15 +0900

    experienceの詳細機能の実装

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -7,6 +7,10 @@ class ExperiencesController < ApplicationController
     @experiences = Experience.page(params[:page]).per(PER_PAGE)
   end
 
+  def show
+    @experience = Experience.find(params[:id])
+  end
+
   def new
     @experience = Experience.new
   end
