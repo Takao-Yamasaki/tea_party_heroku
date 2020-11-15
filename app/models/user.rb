1ee17a5acc559aa3130efda3c51a7e3d47002e41
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2020-11-15 10:54:36 +0900

    予約機能の関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :bookings, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   # first_nameを追記
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_15_000504) do
+ActiveRecord::Schema.define(version: 2020_11_15_014317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "experience_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["experience_id"], name: "index_bookings_on_experience_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "experiences", force: :cascade do |t|
     t.string "title"
@@ -59,4 +68,6 @@ ActiveRecord::Schema.define(version: 2020_11_15_000504) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "experiences"
+  add_foreign_key "bookings", "users"
 end
