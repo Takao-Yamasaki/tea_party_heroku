693e8a37b9f2f03b536e77d57f8a117217b0c0e0
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2020-11-22 21:29:34 +0900

    体験一覧表示の修正

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -4,7 +4,7 @@ class ExperiencesController < ApplicationController
   PER_PAGE = 10
   def index
     @q = Experience.ransack(params[:q])
-    @experiences = @q.result.page(params[:page]).per(PER_PAGE)
+    @experiences = @q.result.page(params[:page]).page(params[:page]).per(PER_PAGE)
   end
 
   def show
