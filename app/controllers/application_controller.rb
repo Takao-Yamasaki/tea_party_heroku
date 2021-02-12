0fa4a52d9c9dffcb26e713ae83b486ec5d423f1f
takao.yamasaki <captain_kaeru@yahoo.co.jp>
2021-02-12 19:59:23 +0900

    topのサービス概要追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name, :image])
   end
+
+  # def authenticate_user
+  #   if @current_user == nill
+  #     flash[:notice] = "ログインが必要です"
+  #     redirect_to("/login")
+  #   end
+  # end
 end
