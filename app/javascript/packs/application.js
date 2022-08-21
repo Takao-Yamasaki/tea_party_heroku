c478a2e2e8a09dd9ac34478944429cc1f5397c88
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-08-22 08:29:26 +0900

    stylesheet_pack_tagをコメントアウト

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,3 +9,5 @@ require("bootstrap/dist/js/bootstrap")
 require("@fortawesome/fontawesome-free/js/all")
 window.$ = window.jQuery = require('jquery');
 require('packs/raty')
+
+import '../stylesheets/application'
