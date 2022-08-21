ae4bf4310d115d4aab007fffc96f4be42a8cb52e
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-08-21 21:19:28 +0900

    seedの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 User.create!([
-  {email: "test@example.com", encrypted_password: "$2a$12$rVVXVzZyZ2WkJiOX8uYI0Ows1dp6sqhPsmhWk6mLh4Nd39MxLIoZ6", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, first_name: "yamasaki", last_name: "takao", image: nil}
+  {email: "yamasaki@example.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, first_name: "yamasaki", last_name: "takao", image: nil}
 ])
