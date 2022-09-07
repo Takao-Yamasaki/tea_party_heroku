849330cbd3ca5d2c5c8207530eebca5b2b6e7676
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-09-07 19:27:42 +0900

    nginxの記述を追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,12 @@
 set -e
 
 # pidファイルを削除する
-rm -f /tea_party/tmp/pids/server.pid
+rm -f /tea_party/tmp/pids/server.pid && bundle exec puma -C config/puma.rb
+
+# db:createとde:seedはfagateの初回起動時のみ実行
+bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rails db:seed
 
 # DockerfileのCMDで渡された`rails s -b 0.0.0.0`を実行
 exec "$@"
